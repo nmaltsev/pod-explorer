f90955d0899597161183898d3b7a5c4a8cd9a660
Nikolai Maltsev <nsmalcev@gmail.com>
2018-11-20 21:19:35 +0100

    update

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -1,8 +1,5 @@
 function toggle($n, show_b) {
 	if (show_b !== null ? show_b : $n.style.display == 'none') {
-		console.log('toggle')
-		console.dir($n)
-		console.dir(show_b)
 		$n.style.display = '';
 	} else {
 		$n.style.display = 'none';
@@ -61,7 +58,11 @@ function $decorate(preferences_c, class_o) {
 				this[privateProperty_s] = value;
 				// TODO something
 
-				console.log('Property %s updated', preferences_c[privateProperty_s])
+				// console.log('Property %s updated', preferences_c[privateProperty_s])
+				if (this instanceof Events) {
+					this.trigger('change:' + preferences_c[privateProperty_s], value);	
+				}
+				
 			}
 		});
 	}
@@ -69,11 +70,88 @@ function $decorate(preferences_c, class_o) {
 	return class_o;
 }
 
+//==================================
+// Events
+//==================================
+
+	function Events(){
+		this._handlers = Object.create(null);
+	};
+	// @memberOf Events - execute event callbacks
+	// @param {Object} options - event options
+	// @return {Bool} - if true - stop event propagation
+	Events.prototype.trigger = function(name){
+		var 	handlers = this._handlers[name],
+				i;
+
+		if(Array.isArray(handlers)){
+			i = handlers.length;
+			while(i-- > 0){
+				if(handlers[i](arguments[1], arguments[2], arguments[3])){
+					return true;
+				}	
+			}
+		}
+		return false;
+	};
+	// @memberOf {Events} - remove all event listeners
+	Events.prototype.destroy = function(){
+		for(var key in this._handlers){
+			this.off(key);
+		}
+	};
+	// @memberOf {Events} - attach callback on change
+	// @param {String} name - property of model
+	// @param {Function} cb - callback
+	Events.prototype.on = function(name, cb){
+		if(!Array.isArray(this._handlers[name])){
+			this._handlers[name] = [];
+		}
+		this._handlers[name].push(cb);
+	};
+	// @memberOf {Events} - deattach event
+	// @param {String} name - property of model
+	// @param {Function} cb - callback
+	Events.prototype.off = function(name, cb){
+		var handlers = this._handlers[name];
+		
+		if(Array.isArray(handlers)){
+			if(cb){
+				var pos = handlers.indexOf(cb);
+				pos != -1 && handlers.splice(pos, 1);
+
+				if(handlers.length == 0){
+					delete this._handlers[name];
+				}
+			}else{
+				handlers.length = 0;
+				delete this._handlers[name];
+			}
+		}
+	};
+	// @memberOf {Events} - attach callback on change
+	// @param {String} name - property of model
+	// @param {Function} cb - callback
+	// @return {Function} handler
+	Events.prototype.once = function(name, cb){
+		if(!Array.isArray(this._handlers[name])){
+			this._handlers[name] = [];
+		}
+		var _cb = function(args){
+			this.off(name, _cb);
+			return cb(args);
+		}.bind(this);
+		this._handlers[name].push(_cb);
+		return _cb;
+	};
+
+
 export {
 	$decorate,
 	bindEvents,
 	findNodes,
 	toggle,
 	cr,
-	emptyNode		
+	emptyNode,
+	Events		
 };
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,18 +1,165 @@
 import * as UITools from './common.js';
 
+
 const popupUri = '/pages/popup.html';
-const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+
 
 const CONTROLS = UITools.findNodes();
-const Model = UITools.$decorate({
-	'_id_n': 'id',
-}, class {
-	constructor (){
 
+// Model Class
+const Model = UITools.$decorate({
+	'_viewedWebId': 'viewedWebId',	// Selected user
+	'_webId': 'webId', // Current authorized user
+}, class Model extends UITools.Events {
+	constructor (){
+		super();
+		let store = $rdf.graph();
+		// Fetcher instance will store all the collected data!
+		this.fetcher = new $rdf.Fetcher(store);
+		this.foaf = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
 	}
+	async populate(webId) {
+		await this.fetcher.load(webId);
+	}
+	getNameOf(webId) {
+		const nameInstance = this.fetcher.store.any($rdf.sym(webId), this.foaf('name'));
+		return nameInstance.value;
+	}
+	getFriendsOf(webId) {
+		return this.fetcher.store.each($rdf.sym(webId), this.foaf('knows'));
+	}
+	
+	// async fetchPublicTypeIndex () {
+	// 	const BOOKMARK = $rdf.Namespace('http://www.w3.org/2002/01/bookmark#');
+	// 	const SOLID = $rdf.Namespace('http://www.w3.org/ns/solid/terms#');
+		
+	// 	const store = $rdf.graph();
+	// 	const fetcher = new $rdf.Fetcher(store);
+	// 	console.dir([
+	// 		$rdf.sym(this.webId), 
+	// 		SOLID('publicTypeIndex'), 
+	// 		null, 
+	// 		$rdf.sym(this.webId.split('#')[0])
+	// 	])
+
+	// 	this.publicTypeIndex = store.any(
+	// 		$rdf.sym(this.webId), 
+	// 		SOLID('publicTypeIndex'), 
+	// 		null, 
+	// 		$rdf.sym(this.webId.split('#')[0]));
+      	
+ //      	// Load the person's data into the store
+ //      	await fetcher.load(this.publicTypeIndex);
+ //      	console.log('this.publicTypeIndex ready')
+ //      	console.dir(this.publicTypeIndex);
+ //      	return;
+ //      // Display their details
+ //      template.profile.bookmarkTypeRegistration = store.any(null, SOLID("forClass"), BOOKMARK("Bookmark"))
+ //      console.log("bookmarkTypeRegistration", template.profile.bookmarkTypeRegistration)
+ //      if (template.profile.bookmarkTypeRegistration && template.profile.bookmarkTypeRegistration.value) {
+ //        template.profile.bookmarkInstance = store.any(template.profile.bookmarkTypeRegistration, SOLID("instance"));
+ //        template.profile.bookmarkInstance = template.profile.bookmarkInstance.value
+ //        fetchBookmarks()
+ //      } else {
+ //        console.log("no bookmark files, creating")
+ //        const query = `INSERT DATA {
+ //            <#Bookmark> a <http://www.w3.org/ns/solid/terms#TypeRegistration> ;
+ //              <http://www.w3.org/ns/solid/terms#forClass> <http://www.w3.org/2002/01/bookmark#Bookmark> ;
+ //              <http://www.w3.org/ns/solid/terms#instance> </public/bookmarks.ttl> .
+ //              <> <http://purl.org/dc/terms/references> <#Bookmark> .
+ //            }`
+ //        // Send a PATCH request to update the source
+ //        console.log("sending PATCH query to",template.profile.publicTypeIndex.value ,query)
+ //        solid.auth.fetch(template.profile.publicTypeIndex.value, {
+ //          method: 'PATCH',
+ //          headers: { 'Content-Type': 'application/sparql-update' },
+ //          body: query,
+ //          credentials: 'include',
+ //        }).then((ret) => {
+ //          console.log("finished", ret)
+ //        })
+ //      }
+ //      render()
+	// }
+	/*async fetchData () {
+		
+
+
+		const store = $rdf.graph();
+		const SOLID = $rdf.Namespace('http://www.w3.org/ns/solid/terms#');
+
+		this.publicTypeIndex = store.any(
+			$rdf.sym(this.webId), 
+			SOLID('publicTypeIndex'), 
+			null, 
+			$rdf.sym(this.webId.split('#')[0])
+		);
+		
+		const BOOKMARK = $rdf.Namespace('https://www.w3.org/2002/01/bookmark#');
+      	
+		console.log('Call fetchData %s', this.webId)
+		console.dir(this.publicTypeIndex);
+		const fetcher = new $rdf.Fetcher(store);
+      // Load the person's data into the store
+      await fetcher.load(template.profile.publicTypeIndex);
+
+
+
+      	const fetcher = new $rdf.Fetcher(store);
+      	const RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+      	const DC = $rdf.Namespace('http://purl.org/dc/terms/')
+      	// Load the person's data into the store
+      	await fetcher.load(this.publicTypeIndex);
+      	// Display their details
+      	let bookmarkTypeRegistration = store.any(null, SOLID("forClass"), BOOKMARK("Bookmark"))
+      	
+      	console.log("bookmarkTypeRegistration", bookmarkTypeRegistration)
+      	console.dir(this);
+
+      	if (bookmarkTypeRegistration && bookmarkTypeRegistration.value) {
+        	let bookmarkInstance = store.any(bookmarkTypeRegistration, SOLID("instance")).value;
+	      	// Load the person's data into the store
+	      	console.log("bookmarkInstance", template.profile.bookmarkInstance)
+	      	console.dir(this)
+      	
+      		await fetcher.load(template.profile.bookmarkInstance);
+      		// Display their details
+      		const bookmarks = store.statementsMatching(null, RDF("type"), BOOKMARK("Bookmark"));
+      		console.log("Bookmarks", bookmarks);
+      	
+	      	if (bookmarks && bookmarks.length) {
+	        	template.bookmarks = []
+	        	for (var i = 0; i < bookmarks.length; i++) {
+		          	let bookmark = bookmarks[i]
+		          	let subject = bookmark.subject
+		          	let title = store.any(subject, DC('title'))
+		          	let created = store.any(subject, DC('created'))
+		          	let recall = store.any(subject, BOOKMARK('recall'))
+	          		
+	          		if (subject && recall && created && title) {
+			            template.bookmarks.push({
+			              "subject": subject.value,
+			              "recall": recall.value,
+			              "created": created.value,
+			              "title": title.value
+			            })
+	          		}
+	          		console.log("bookmark " + i, bookmark)
+	        	}
+	        	
+	        	console.log("template.bookmarks", template.bookmarks)
+	        	// render()
+	      	}
+      		// console.log()
+      	}
+      	// render()		
+	}*/
 });
+
+
 const MODEL = new Model();
 
+// DOM event listeners
 UITools.bindEvents(CONTROLS, {
 	'onclick loginBtn': function() {
 		solid.auth.popupLogin({ popupUri });
@@ -22,46 +169,69 @@ UITools.bindEvents(CONTROLS, {
 	},
 	'onsubmit profileSelectForm': function(e) {
 		e.preventDefault();
-		loadProfile();
+		MODEL.viewedWebId = CONTROLS.profileField.value;
 	},
+	'onsubmit writeReview': function(e) {
+
+	}
 });
 
-async function loadProfile() {
-	// Set up a local data store and associated data fetcher
-	const store = $rdf.graph();
-	const fetcher = new $rdf.Fetcher(store);
+// Model event listeners
+MODEL.on('change:viewedWebId', async function(webId){
+	await MODEL.populate(webId);
 
-	// Load the person's data into the store
-	await fetcher.load(MODEL.id);
+	let 	fullName = MODEL.getNameOf(webId), 
+			friends = MODEL.getFriendsOf(webId);
 
-	// Display their details
-	const fullName = store.any($rdf.sym(MODEL.id), FOAF('name'));
-  	const friends = store.each($rdf.sym(MODEL.id), FOAF('knows'));
-
-  	console.log('loadProfile: %s name: %s', MODEL.id, FOAF('name'));
-
-  	CONTROLS.fullNameLabel.textContent = fullName ? fullName.value : '';
+	CONTROLS.fullNameLabel.textContent = fullName || '';
+	CONTROLS.profileField.value = webId;
   	UITools.emptyNode(CONTROLS.friends);
   
 	friends.forEach(async (friend) => {
-		await fetcher.load(friend);
-		const fullName = store.any(friend, FOAF('name'));
+		await MODEL.populate(friend);
+
+		const fullName = MODEL.getNameOf(friend);
 
 		let $a = UITools.cr('a');
 		let $li = UITools.cr('li');
 
-		$a.textContent = fullName ? fullName.value : friend.value;
+		$a.textContent = fullName ? fullName : friend.value;
 		$a.onclick = function() {
-			MODEL.id = friend.value;
-			// TODO move in model change handler
-			CONTROLS.profileField.value = friend.value;
-
-			loadProfile();
+			MODEL.viewedWebId = friend.value;
 		};
 		$li.appendChild($a);
 		CONTROLS.friends.appendChild($li);
 	});
-}
+});
+
+window.model = MODEL;
+
+
+
+// async function sendReview() {
+//     document.getElementById('modal').classList.remove('is-active')
+//         let id = "#" + Math.random()
+//         let uri = document.getElementById('uri').value
+//         let title = document.getElementById('title').value
+//         let source = template.profile.bookmarkInstance
+//         let date = new Date().toISOString();
+//         const query = ` INSERT DATA {
+//             <${id}> a <https://www.w3.org/2002/01/bookmark#Bookmark> ;
+//             <http://purl.org/dc/terms/title>   """${title}""" ;
+//             <http://xmlns.com/foaf/0.1/maker>   <${template.profile.webId}> ;
+//             <http://purl.org/dc/terms/created>  "${date}"^^<http://www.w3.org/2001/XMLSchema#dateTime> ;
+//             <https://www.w3.org/2002/01/bookmark#recall> <${uri}> .
+//           }`
+//         // Send a PATCH request to update the source
+//         console.log("query", query)
+//         solid.auth.fetch(source, {
+//           method: 'PATCH',
+//           headers: { 'Content-Type': 'application/sparql-update' },
+//           body: query,
+//           credentials: 'include',
+//         });
+// }
+
 
 // Update components to match the user's login status
 solid.auth.trackSession(session => {
@@ -72,13 +242,11 @@ solid.auth.trackSession(session => {
 	UITools.toggle(CONTROLS.loadingBlock, false);
 
 	if (loggedIn_b) {
-		MODEL.id = session.webId;
+		MODEL.viewedWebId = session.webId;
+		MODEL.webId = session.webId;
+		MODEL.populate(MODEL.webId);
 
 		CONTROLS.userLabel.textContent = session.webId;
-
-		if (!CONTROLS.profileField.value) {
-			CONTROLS.profileField.value = session.webId;
-		}
 	} else {
 		CONTROLS.userLabel.textContent = '';		
 	}
