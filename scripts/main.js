9668aea05d52092b37d1afbc48b41a514a1fed46
Nikolai Maltsev <nsmalcev@gmail.com>
2018-11-23 16:03:16 +0100

    upd

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,15 @@
 import * as UITools from './common.js';
+import {Model, Hotel} from './model.js';
 
 const popupUri = 'pages/popup.html';
-const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+
 
 const CONTROLS = UITools.findNodes();
-const Model = UITools.$decorate({
-	'_id_n': 'id',
-}, class {
-	constructor (){
 
-	}
-});
+
 const MODEL = new Model();
 
+// DOM event listeners
 UITools.bindEvents(CONTROLS, {
 	'onclick loginBtn': function() {
 		solid.auth.popupLogin({ popupUri });
@@ -22,49 +19,141 @@ UITools.bindEvents(CONTROLS, {
 	},
 	'onsubmit profileSelectForm': function(e) {
 		e.preventDefault();
-		loadProfile();
+		MODEL.viewedWebId = CONTROLS.profileField.value;
+	},
+	'onsubmit bookmarkForm': function(e) {
+		e.preventDefault();
+		MODEL.sendBookmark(
+			MODEL.generateDocumentUID(),
+			CONTROLS.bookmarkFormUrlField.value,
+			CONTROLS.bookmarkFormTitleField.value
+		);
+	}, 
+	'onreset bookmarkForm': function(e) {
+		e.preventDefault();
+		CONTROLS.bookmarkFormTitleField.value = '';
+		CONTROLS.bookmarkFormUrlField.value = '';
+	},
+	'onsubmit reviewForm': async function(e) {
+		e.preventDefault();
+		let hotel = new Hotel(CONTROLS.reviewFormHotelField.value);
+		
+		hotel.setAddress(CONTROLS.reviewFormCityField.value, CONTROLS.reviewFormCountryField.value)
+
+		await MODEL.sendReview(
+			MODEL.generateDocumentUID(),
+			CONTROLS.reviewFormTitleField.value, 
+			CONTROLS.reviewFormDescriptionField.value,
+			hotel
+		);
+		CONTROLS.reviewForm.reset();		
+	},
+	'onreset reviewForm': function(e) {
+		e.preventDefault();
+		CONTROLS.reviewFormCityField.value = '';
+		CONTROLS.reviewFormHotelField.value = '';
+		CONTROLS.reviewFormCountryField.value = '';
+		CONTROLS.reviewFormTitleField.value = ''; 
+		CONTROLS.reviewFormDescriptionField.value = '';
 	},
 });
 
-async function loadProfile() {
-	// Set up a local data store and associated data fetcher
-	const store = $rdf.graph();
-	const fetcher = new $rdf.Fetcher(store);
+// Model event listeners
+// When viewedWebId is changed we download and render all related information
+MODEL.on('change:viewedWebId', async function(model, webId){
+	await MODEL.populate(webId);
 
-	// Load the person's data into the store
-	await fetcher.load(MODEL.id);
+	let 	fullName = MODEL.getNameOf(webId), 
+			friends = MODEL.getFriendsOf(webId);
 
-	// Display their details
-	const fullName = store.any($rdf.sym(MODEL.id), FOAF('name'));
-  	const friends = store.each($rdf.sym(MODEL.id), FOAF('knows'));
-
-  	console.log('loadProfile: %s name: %s', MODEL.id, FOAF('name'));
-
-  	CONTROLS.fullNameLabel.textContent = fullName ? fullName.value : '';
+	CONTROLS.fullNameLabel.textContent = fullName || '';
+	CONTROLS.profileField.value = webId;
   	UITools.emptyNode(CONTROLS.friends);
   
 	friends.forEach(async (friend) => {
-		await fetcher.load(friend);
-		const fullName = store.any(friend, FOAF('name'));
+		await MODEL.populate(friend);
+
+		const fullName = MODEL.getNameOf(friend);
 
 		let $a = UITools.cr('a');
 		let $li = UITools.cr('li');
 
-		$a.textContent = fullName ? fullName.value : friend.value;
+		$a.textContent = fullName ? fullName : friend.value;
 		$a.onclick = function() {
-			MODEL.id = friend.value;
-			// TODO move in model change handler
-			CONTROLS.profileField.value = friend.value;
-
-			loadProfile();
+			MODEL.viewedWebId = friend.value;
 		};
 		$li.appendChild($a);
 		CONTROLS.friends.appendChild($li);
 	});
-}
+});
+MODEL.on('change:bookmarks', async function(model, bookmarks){
+	UITools.emptyNode(CONTROLS.bookmarkList);
+	
+	var $a, $li;
+	// TODO create document fragment
+	for (var i = 0; i < bookmarks.length; i++) {
+		$a = UITools.cr('a');
+		$li = UITools.cr('li');
+		$a.textContent = bookmarks[i].title;
+		$a.setAttribute('href', bookmarks[i].recall);
+		$a.setAttribute('target', '_blank');
+
+		$li.appendChild($a);
+		CONTROLS.bookmarkList.appendChild($li);	
+	}	
+});
+// When bookmark form succesfully  submitted we can clean form 
+MODEL.on('bookmarkSended', function(model) {
+	CONTROLS.bookmarkForm.reset();
+	model.fetchBookmarks(true);
+});
+MODEL.on('reviewSended', function(model) {
+	CONTROLS.reviewForm.reset();
+	model.fetchReviews(true);
+});
+// Trouble handler
+MODEL.on('change:troubles', function(model, trouble) {
+	if (trouble) {
+		// TODO if 401 error switch on authorization page
+		console.warn('Catch troubles')
+		console.dir(trouble);
+	} else {
+
+	}
+});
+MODEL.on('change:reviews', async function(model, reviews){
+	UITools.emptyNode(CONTROLS.reviewList);
+
+	var $li, review, title_s, r;
+
+	// // TODO create document fragment
+	for (var i = 0; i < reviews.length; i++) {
+		$li = UITools.cr('li');
+		r = reviews[i];
+
+		if (r.hotel) {
+			title_s = r.hotel.getShortName();
+		} else {
+			title_s = '';
+		}
+
+		$li.className = 'review-item';
+		$li.insertAdjacentHTML('beforeEnd', 
+		`<h3 class="review-item_place">${title_s}</h3>
+		<p class="review-item_title">${r.name}</p>
+		<pre class="review-item_description">${r.description}</pre>
+		<p class="review-item_publication-time">${r.datePublished}</p>`);
+
+
+		CONTROLS.reviewList.appendChild($li);	
+	}	
+});
+
+
+window.model = MODEL;
 
 // Update components to match the user's login status
-solid.auth.trackSession(session => {
+solid.auth.trackSession(async (session) => {
 	const loggedIn_b = !!session;
 	
 	UITools.toggle(CONTROLS.logoutBlock, loggedIn_b);
@@ -72,15 +161,14 @@ solid.auth.trackSession(session => {
 	UITools.toggle(CONTROLS.loadingBlock, false);
 
 	if (loggedIn_b) {
-		MODEL.id = session.webId;
+		MODEL.viewedWebId = session.webId;
+		MODEL.webId = session.webId;
+		await MODEL.populate(MODEL.webId);
 
 		CONTROLS.userLabel.textContent = session.webId;
 
-		if (!CONTROLS.profileField.value) {
-			CONTROLS.profileField.value = session.webId;
-		}
+		MODEL.fetchPublicTypeIndex();
 	} else {
 		CONTROLS.userLabel.textContent = '';		
 	}
 });
-
---
+++ scripts/model.js
@@ -0,0 +1,396 @@
+import * as UITools from './common.js';
+
+class Hotel {
+	constructor (name_s) {
+		this.name_s = name_s;
+	}
+	setAddress (locality_s, country_s){
+		this.locality_s = locality_s;
+		this.country_s = country_s;
+	}
+	getShortName() {
+		let out = this.name_s;
+		if (this.locality_s) out += ', ' + this.locality_s;
+		if (this.country_s) out += ', ' + this.country_s;
+
+		return out;
+	}
+}
+
+// Model Class
+const Model = UITools.$decorateWatchers([
+	'viewedWebId',	// Selected user
+	'webId', // Current authorized user
+	'bookmarks',
+	'reviews',
+	'troubles',
+], class Model extends UITools.Events {
+	constructor (){
+		super();
+		let store = $rdf.graph();
+		// Fetcher instance will store all the collected data!
+		this.fetcher = new $rdf.Fetcher(store);
+		this.updater = new $rdf.UpdateManager(store);
+		this.namespace = {
+			foaf: $rdf.Namespace('http://xmlns.com/foaf/0.1/'),
+			bookmark: $rdf.Namespace('http://www.w3.org/2002/01/bookmark#'),
+			solid: $rdf.Namespace('http://www.w3.org/ns/solid/terms#'),
+			rdf: $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+      		dc: $rdf.Namespace('http://purl.org/dc/terms/'),
+      		schemaOrg: $rdf.Namespace('https://schema.org/'),
+      		review: $rdf.Namespace('https://schema.org/Review#'),
+		};
+		this.sessionId = this.generateRandToken(2);
+	}
+
+	generateDocumentUID (){
+		return '#' + this.sessionId + '.' + this.generateRandToken(2);
+	}
+
+	async populate(webId) {
+		this.troubles = null;
+		try {
+			await this.fetcher.load(webId);	
+		} catch (e) {
+			this.troubles = e;
+		}
+	}
+
+	getNameOf(webId) {
+		const nameInstance = this.fetcher.store.any($rdf.sym(webId), this.namespace.foaf('name'));
+		return nameInstance.value;
+	}
+
+	getFriendsOf(webId) {
+		return this.fetcher.store.each($rdf.sym(webId), this.namespace.foaf('knows'));
+	}
+
+	// Trouble: it brings cached variant, do not try to upload!!
+	
+	async fetchPublicTypeIndex () {
+		this.publicTypeIndex = this.fetcher.store.any(
+			$rdf.sym(this.webId), 
+			this.namespace.solid('publicTypeIndex'), 
+			null, 
+			$rdf.sym(this.webId.split('#')[0]));
+
+		// Load the person's data into the store
+		await this.fetcher.load(this.publicTypeIndex);
+
+		// Display bookmarks details
+		this.bookmarkTypeRegistration = this.fetcher.store.any(
+			null, 
+			this.namespace.solid('forClass'), 
+			this.namespace.bookmark('Bookmark')
+		);
+ 
+ 		if (this.bookmarkTypeRegistration && this.bookmarkTypeRegistration.value) {
+			this.bookmarkInstance = this.fetcher.store.any(
+				this.bookmarkTypeRegistration, 
+				this.namespace.solid('instance')
+			);
+
+			// Subscribe on updation
+			this.updater.addDownstreamChangeListener(this.bookmarkInstance.doc(), async () => {
+				console.log('[BOOKMARKS UPDATED]');
+				console.dir(arguments)
+
+				// If updation is happened, we make force reload of bookmarks list 
+				await this.reloadBookmarks();
+				// // Read bookmarks from bookmarks.ttl file
+				this.bookmarks = this.extractBookmarks();
+				// this.fetchBookmarks(true);
+			});
+
+			await this.fetchBookmarks();
+		} else {
+			console.log('no bookmark files, creating')
+			const query = `INSERT DATA {
+			<#Bookmark> a <http://www.w3.org/ns/solid/terms#TypeRegistration> ;
+				<http://www.w3.org/ns/solid/terms#forClass> <http://www.w3.org/2002/01/bookmark#Bookmark> ;
+				<http://www.w3.org/ns/solid/terms#instance> </public/bookmarks.ttl> .
+				<> <http://purl.org/dc/terms/references> <#Bookmark> .
+			}`
+			// Send a PATCH request to update the source
+			console.log('sending PATCH query to', this.publicTypeIndex.value ,query)
+			solid.auth.fetch(this.publicTypeIndex.value, {
+				method: 'PATCH',
+				headers: { 'Content-Type': 'application/sparql-update' },
+				body: query,
+				credentials: 'include',
+			}).then((ret) => {
+				console.log("finished", ret)
+			});
+
+			this.bookmarks = [];
+		}
+
+		// Get review details
+		this.reviewTypeRegistration = this.fetcher.store.any(
+			null, 
+			this.namespace.solid('forClass'), 
+			// this.namespace.schemaOrg('Review')
+			this.namespace.review('Review')
+		);
+
+		if (this.reviewTypeRegistration && this.reviewTypeRegistration.value) {
+			this.reviewInstance = this.fetcher.store.any(
+				this.reviewTypeRegistration, 
+				this.namespace.solid('instance')
+			);			
+
+			// Subscribe on updation
+			this.updater.addDownstreamChangeListener(this.reviewInstance.doc(), async () => {
+				console.log('Reviews updated');
+				this.fetchReviews(true);
+			});
+
+			await this.fetchReviews();
+		} else { // There is no reviews.ttl, create it
+			this.createReviewFile();
+			this.reviews = [];
+		}
+	}
+
+	async reloadBookmarks() {
+		if (this.bookmarkInstance) {
+			await this.fetcher.load(this.bookmarkInstance, {force: true});	
+		}
+	}
+
+	async fetchBookmarks (isForce) {
+		let bookmarkInstance = this.fetcher.store.any(
+			this.bookmarkTypeRegistration, 
+			this.namespace.solid('instance')
+		);
+		let loadProps = {};
+		if (isForce) loadProps.force = true;
+		// Load the person's data into the store
+
+		try {
+			await this.fetcher.load(this.bookmarkInstance, loadProps);	
+		} catch (e) {
+			await solid.auth.fetch(this.bookmarkInstance, {
+				method : 'PATCH',
+				headers : {'content-type' : 'application/sparql-update'},
+				body : ''
+			});
+		}
+		// Display their details
+		this.bookmarks = this.extractBookmarks();
+	}
+
+	extractBookmarks() {
+		// Display their details
+		const bookmarksInstance = this.fetcher.store.statementsMatching(
+			null, 
+			this.namespace.rdf('type'), 
+			this.namespace.bookmark('Bookmark')
+		);
+
+		if (bookmarksInstance && bookmarksInstance.length) {
+			let bookmarks = []
+
+			for (var i = 0; i < bookmarksInstance.length; i++) {
+				let subject = bookmarksInstance[i].subject;
+
+				let title = this.fetcher.store.any(subject, this.namespace.dc('title'))
+				let created = this.fetcher.store.any(subject, this.namespace.dc('created'))
+				let recall = this.fetcher.store.any(subject, this.namespace.bookmark('recall'))
+
+				if (subject && recall && created && title) {
+					bookmarks.push({
+						subject: subject.value,
+						recall: recall.value,
+						created: created.value,
+						title: title.value
+					});
+				}
+				// console.log("bookmark " + i, bookmarksInstance[i])
+			}
+
+			return bookmarks;
+		} else {
+			return [];
+		}
+	}
+
+	async sendBookmark(id_s, uri_s, title_s) {
+		let source = this.bookmarkInstance.value;
+		let date_s = new Date().toISOString();
+		const query = `INSERT DATA {
+			<${id_s}> a <http://www.w3.org/2002/01/bookmark#Bookmark> ;
+			<http://purl.org/dc/terms/title>   """${title_s}""" ;
+			<http://xmlns.com/foaf/0.1/maker>   <${this.webId}> ;
+			<http://purl.org/dc/terms/created>  "${date_s}"^^<http://www.w3.org/2001/XMLSchema#dateTime> ;
+			<http://www.w3.org/2002/01/bookmark#recall> <${uri_s}> .
+		}`;
+		// Send a PATCH request to update the source
+		solid.auth.fetch(source, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/sparql-update' },
+			body: query,
+			credentials: 'include',
+		}).then((ret) => {
+			this.trigger('bookmarkSended', this);
+		}).catch(err => {
+			console.log("error updating", source, err)
+		});
+	}
+
+	generateRandToken(n) {
+		return ~~((1 << n *10) * Math.random());
+	}
+
+	escape4rdf(property) {
+		return property.replace(/\"/g, '\'');
+	}
+
+	// Id must be beginned with `#`
+	async sendReview(id_s, title_s, description_s, hotel){
+		// https://schema.org/Review
+		let source = this.reviewInstance.value;
+		let date_s = new Date().toISOString();
+		const query = `INSERT DATA {
+			@prefix schema: <https://schema.org/> .
+			@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+			@prefix foaf: <http://xmlns.com/foaf/0.1/>.
+
+			<${id_s}> a schema:Review ;
+			foaf:maker <${this.webId}>;
+			schema:author "Ellie"^^xsd:string ;
+			schema:datePublished "${date_s}"^^schema:dateTime ;
+			schema:description """${this.escape4rdf(description_s)}"""^^xsd:string ;
+			schema:name """${this.escape4rdf(title_s)}"""^^xsd:string ;
+			schema:reviewRating [
+				a schema:Rating ;
+				schema:bestRating "5"^^xsd:string ;
+				schema:ratingValue "1"^^xsd:string ;
+				schema:worstRating "1"^^xsd:string
+			] ;
+			schema:hotel [
+				a schema:Hotel ;
+				schema:name """${this.escape4rdf(hotel.name_s)}"""^^xsd:string ;
+				schema:address [
+					a schema:PostalAddress ;
+					schema:addressCountry "${this.escape4rdf(hotel.country_s)}"^^xsd:string ;
+					schema:addressLocality "${this.escape4rdf(hotel.locality_s)}"^^xsd:string ;
+					schema:addressRegion "Alpes-Maritimes"^^xsd:string ;
+					schema:postalCode "006200"^^xsd:string ;
+					schema:streetAddress "Rue de France 20"^^xsd:string
+				] ;
+			] .
+		}`;
+
+		// Send a PATCH request to update the source
+		solid.auth.fetch(source, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/sparql-update' },
+			body: query,
+			credentials: 'include',
+		}).then((ret) => {
+			this.trigger('reviewSended', this);
+		}).catch(err => {
+			console.log("error updating", source, err)
+		});
+
+	}
+
+	createReviewFile() {
+		// const query = `INSERT DATA {
+		// 	<#Review> a <http://www.w3.org/ns/solid/terms#TypeRegistration> ;
+		// 		<http://www.w3.org/ns/solid/terms#forClass> <https://schema.org/Review#Review> ;
+		// 		<http://www.w3.org/ns/solid/terms#instance> </public/reviews.ttl> .
+		// 		<> <http://purl.org/dc/terms/references> <#Review> .
+		// 	}`;
+		const query = `INSERT DATA {
+			<#Review> a <http://www.w3.org/ns/solid/terms#TypeRegistration> ;
+				<http://www.w3.org/ns/solid/terms#forClass> <https://schema.org/Review> ;
+				<http://www.w3.org/ns/solid/terms#instance> </public/reviews.ttl> .
+				<> <http://purl.org/dc/terms/references> <#Review> .
+			}`;
+		// Send a PATCH request to update the source
+		console.log('sending PATCH query to', this.publicTypeIndex.value ,query)
+		solid.auth.fetch(this.publicTypeIndex.value, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/sparql-update' },
+			body: query,
+			credentials: 'include',
+		}).then((ret) => {
+			console.log("finished", ret)
+		});
+	}
+
+	deleteReviewFile() {
+		// It doesn't work
+		this.fetcher.webOperation('DELETE', this.reviewInstance.uri);
+	}
+
+	async fetchReviews (isForce) {
+		let loadProps = {};
+		if (isForce) loadProps.force = true;
+
+		try {
+			await this.fetcher.load(this.reviewInstance, loadProps);	
+		} catch (e) {
+			await solid.auth.fetch(this.reviewInstance, {
+				method : 'PATCH',
+				headers : {'content-type' : 'application/sparql-update'},
+				body : ''
+			});
+		}
+  		// Display their details
+		this.reviews = this.extractReviews();
+	}
+
+	extractReviews() {
+		const reviewStore = this.fetcher.store.statementsMatching(
+			null, 
+			this.namespace.rdf('type'), 
+			this.namespace.schemaOrg('Review')
+		);
+
+		if (reviewStore && reviewStore.length) {
+			let reviews = [];
+
+			for (var i = 0; i < reviewStore.length; i++) {
+				let subject = reviewStore[i].subject;
+				let review = {};
+				let author = this.fetcher.store.any(subject, this.namespace.schemaOrg('author')); 
+				let datePublished = this.fetcher.store.any(subject, this.namespace.schemaOrg('datePublished'));
+				let description = this.fetcher.store.any(subject, this.namespace.schemaOrg('description'));
+				let name = this.fetcher.store.any(subject, this.namespace.schemaOrg('name'));
+				let hotelInstance = this.fetcher.store.any(subject, this.namespace.schemaOrg('hotel'));
+
+				if (author) review.author = author.value;
+				if (datePublished) review.datePublished = new Date(datePublished.value);
+				if (description) review.description = description.value;
+				if (name) review.name = name.value;
+
+				if (hotelInstance) {
+					// TODO transform in Class
+					let hotelName = this.fetcher.store.any(hotelInstance, this.namespace.schemaOrg('name'));
+					let addressInstance = this.fetcher.store.any(hotelInstance, this.namespace.schemaOrg('address'));
+
+					review.hotel = new Hotel(hotelName ? hotelName.value : '');
+	
+					if (addressInstance) {
+						let country = this.fetcher.store.any(addressInstance, this.namespace.schemaOrg('addressCountry'));
+						let locality = this.fetcher.store.any(addressInstance, this.namespace.schemaOrg('addressLocality'));
+						review.hotel.setAddress(locality && locality.value, country && country.value);
+					}
+				}
+				reviews.push(review);
+			}
+
+			return reviews;
+		} else {
+			return [];
+		}
+	}
+});
+
+export {
+	Model,
+	Hotel 
+};
