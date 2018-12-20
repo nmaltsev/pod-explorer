cc804d986641e6497e51f2608e351f3ab324581f
Nikolai Maltsev <nsmalcev@gmail.com>
2018-12-20 19:12:34 +0100

    add hash model

--- static/scripts/index.controller.js
+++ static/scripts/index.controller.js
@@ -1,24 +1,26 @@
 import * as UITools from './common.js';
 import {Model, Hotel} from './model.js';
 import {Storage, StorageException} from './storage.js';
-import {ACL_ACCESS_MODES, createSafeRuleset, Ruleset} from './acl_manager.js';
+// import {ACL_ACCESS_MODES, createSafeRuleset, Ruleset} from './acl_manager.js';
 import {PopupBuilder} from '../libs/popup_builder.js';
 import {createContentPopup} from '../utils/content.popup.js';
 import {createTextPopup} from '../utils/text.popup.js';
+import {createRulesetPopup} from './popups/ruleset.popup.js';
+import {HashModel} from './models/hash.model.js';
+
 
 const popupUri = '/pages/popup.html';
 
 
 const CONTROLS = UITools.findNodes();
 const _storage = new Storage();
+const _hash = new HashModel(['url']); 
 
-
+window._hash = _hash;
 window._storage = _storage;
 window._controls = CONTROLS;
 
 function showResourceContent(resourceLink, resourceData) {
-	console.log('showResourceContent');
-	console.dir(resourceData);
 	if (resourceData.type.indexOf('text/') != -1) {
 		_storage.getContent(resourceLink).then((data) => {
 			createContentPopup({
@@ -102,7 +104,8 @@ UITools.bindEvents(CONTROLS, {
 				break;
 			case 'info': 
 				_storage.getACLInfo(href_s).then(function(d){
-					openRulesetPopup(d);
+					// openRulesetPopup(d);
+					createRulesetPopup(d).open();
 				});
 				break;
 			case 'download': 
@@ -159,7 +162,8 @@ UITools.bindEvents(CONTROLS, {
 
 	'click btnShowInfo': async function(){
 		_storage.getACLInfo(_storage.url).then(function(d){
-			openRulesetPopup(d);
+			// openRulesetPopup(d);
+			createRulesetPopup(d).open();
 		});
 	},
 	
@@ -197,6 +201,8 @@ _storage.bindEvents({
 	'change:url': function(model, url, prevUrl){
 		console.log('[change:url] %s', url);
 		CONTROLS.navigationUrl.value = url;
+		_hash.update('url', url);
+
 		model.showFolder(url);
 		model.prevUrl = prevUrl;
 
@@ -238,7 +244,15 @@ _storage.bindEvents({
 			CONTROLS.navigationTableBody.appendChild($tr);	
 		});	
 	}
-})
+});
+
+_hash.on('url', function(m, url_s){
+	console.log('URL: %s', url_s);
+	_storage.url = url_s;
+});
+_hash.init();
+
+// Attention: _hash.init call must be before
 // Update components to match the user's login status
 solid.auth.trackSession(async (session) => {
 	const loggedIn_b = !!session;
@@ -250,176 +264,10 @@ solid.auth.trackSession(async (session) => {
 	if (loggedIn_b) {
 		CONTROLS.userLabel.textContent = session.webId;
 
-		_storage.url = $rdf.sym(session.webId).site().uri;
+		_storage.url = _hash.state.url || $rdf.sym(session.webId).site().uri;
 		_storage.webId = session.webId;
 		_storage.$webId = $rdf.sym(session.webId);
 	} else {
 		CONTROLS.userLabel.textContent = '';		
 	}
 });
-
-
-function addNewRuleset(index) {
-
-}
-
-function openRulesetPopup(d) {
-	console.log('openRulesetPopup');
-	console.dir(d);
-
-	let popup = new PopupBuilder({
-		className: 'm3-dialog __size-a',
-		content: `
-			<form data-co="form" class="">
-				<div class="mb4" data-co="ruleset-list"></div>
-				
-				<div class="btnline">
-					<button class="basebtn __gray" data-co="add">Add new ruleset</button>
-					<button type="reset" class="basebtn __gray">Close</button>
-					<button type="submit" class="basebtn __blue">Update</button>
-				</div>
-			</form>
-		`,
-		events: {
-			'form click': function(e) {
-				if (e.target.dataset.action == 'removeRuleset') {
-					e.stopPropagation();
-					let index = parseInt(e.target.dataset.index);
-					this.rulesetControls[index] = 0;
-					e.target.parentNode.remove();
-				}
-			},
-			'add click': function(e) {
-				e.preventDefault();
-				this.rulesetControls.push(
-					this.renderRuleset(
-						createSafeRuleset(d.uri, _storage.webId, d.aclUri), 
-						this.rulesetControls.length
-					)
-				);
-			},
-			'form submit': function(e){
-				e.preventDefault();
-				let rulesets = this.rulesetControls.filter(function(r){return r!=0;}).map((controls) => {
-					let ruleset = new Ruleset(controls.fieldRulesetId.value);
-
-					ruleset.mode = [];
-
-					if (controls.checkboxRead.checked) ruleset.mode.push(ACL_ACCESS_MODES.read);
-					if (controls.checkboxWrite.checked) ruleset.mode.push(ACL_ACCESS_MODES.write);
-					if (controls.checkboxControl.checked) ruleset.mode.push(ACL_ACCESS_MODES.control);
-					if (controls.checkboxAppend.checked) ruleset.mode.push(ACL_ACCESS_MODES.append);
-
-					ruleset.accessTo = this.parseArray(controls.accessTo.value);
-					ruleset.agent = this.parseArray(controls.agent.value);
-					ruleset.setAgentClass(this.parseArray(controls.agentClass.value));
-					ruleset.setGroup(this.parseArray(controls.group.value));
-					ruleset.defaultForNew = this.parseArray(controls.defaultForNew.value);
-
-					return ruleset;
-				});
-
-
-				_storage.updateACL(d.aclUri, rulesets).then(() => {
-					this.close();	
-				});
-				// _storage.setACL(d.aclUri, d.uri).then(() => {
-				// 	this.close();	
-				// });
-			},
-			'form reset': function(e){
-				e.preventDefault();
-
-				this.close();
-			}
-		},
-	}, {
-		parseArray: function(str) {
-			let s = str.replace(/\s/g, '');
-			return s ? s.split(',') : [];
-		},
-		onopen: function(view) {
-			this.rulesetControls = d.rulesets.map(function(ruleset, rulesetIndex) {
-				return view.renderRuleset(ruleset, rulesetIndex);
-			});
-		},
-		onclose: function(view){
-
-		},
-		rulesetControls: [],
-		renderRuleset: function(ruleset, rulesetIndex) {
-			let $tr = UITools.cr('div');
-
-			$tr.className = 'ruleset-item';
-			
-			$tr.insertAdjacentHTML('beforeEnd', 
-			`	<div class="ruleset-item_remove" data-action="removeRuleset" data-index="${rulesetIndex}">[x]</div>
-				<label>
-					<p class="">Ruleset id:</p>
-					<input class="basefield" type="text" data-co="field-ruleset-id" required/>
-				</label>
-				<div class="mb2">
-					<p>Access mode:</p>
-					<label>
-						<input type="checkbox" data-co="checkbox-read"/><span>Read</span>
-					</label>
-					<label>
-						<input type="checkbox" data-co="checkbox-write"/><span>Write</span>
-					</label>
-					<label>
-						<input type="checkbox" data-co="checkbox-append"/><span>Append</span>
-					</label>
-					<label>
-						<input type="checkbox" data-co="checkbox-control"/><span>Control</span>
-					</label>
-				</div>
-				<div>
-					<p>Access to (Enter url of resource - folder or file):</p>
-					<textarea class="basefield" data-co="accessTo"></textarea>
-				</div>
-				<div>
-					<p>Granted to users (Enter webId):</p>
-					<textarea class="basefield" data-co="agent"></textarea>
-				</div>
-				<div>
-					<p>Granted to groups (Enter group url):</p>
-					<textarea class="basefield" data-co="group"></textarea>
-				</div>
-				<div>
-					<p>agentClass (http://xmlns.com/foaf/0.1/Agent for non authorized users):</p>
-					<textarea class="basefield" data-co="agentClass"></textarea>
-				</div>
-				<div>
-					<p>defaultForNew (Enter url of resource - folder or file):</p>
-					<textarea class="basefield" data-co="defaultForNew"></textarea>
-				</div>
-			`);
-
-			this.controls.rulesetList.appendChild($tr);	
-			let controls = UITools.findNodes($tr);
-		
-			controls.fieldRulesetId.value = ruleset.id;
-			console.log('ruleset.mode');
-			console.dir(ruleset.mode);
-			console.dir(ACL_ACCESS_MODES);
-
-			(ruleset.mode || []).forEach(function(mode) {
-				if (mode.value == ACL_ACCESS_MODES.read.value) controls.checkboxRead.checked = true;
-				if (mode.value == ACL_ACCESS_MODES.write.value) controls.checkboxWrite.checked = true;
-				if (mode.value == ACL_ACCESS_MODES.control.value) controls.checkboxControl.checked = true;
-				if (mode.value == ACL_ACCESS_MODES.append.value) controls.checkboxAppend.checked = true;
-			});
-
-			controls.accessTo.value = (ruleset.accessTo || []).join(',\n');
-			controls.agent.value = (ruleset.agent || []).join(',\n');
-			controls.agentClass.value = (ruleset.agentClass || []).join(',\n');
-			controls.defaultForNew.value = (ruleset.defaultForNew || []).join(',\n');
-			controls.group.value = (ruleset.agentGroup || []).join(',\n');
-
-
-			return controls;	
-		},
-	});
-
-	popup.open();
-}
